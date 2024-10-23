b94f50d155c76bdc964fa8258ea223b76f61f00b
SurendarYama <surendaryama@outlook.com>
2024-10-23 22:08:29 +0530

    author name color added.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,11 @@ module.exports = {
     "./public/**/*.html",
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
+  theme: {
+    fontFamily: {
+      sure: ["sure"],
+      "sure-i": ["sure-italic"],
+    },
+  },
   // more options here
 };
