527bf7e60d746451d56a13962f45971638e8b453
Surendar Yama <surendaryama@outlook.com>
2025-03-28 10:04:52 +0530

    Website V2 Done unused imports removed.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,14 @@
 // @ts-nocheck
-export {default as Comma } from "./comma.svelte";
-export {default as SemiColon } from "./semicolon.svelte";
-export {default as OpenBracket} from "./open-bracket.svelte";
-export {default as CloseBracket} from "./close-bracket.svelte";
-export {default as OpenFlowerBracket} from "./open-flower-bracket.svelte";
-export {default as CloseFlowerBracket} from "./close-flower-bracket.svelte";
-export {default as SingleQuote} from "./single-quote.svelte";
-export {default as DoubleQuote} from "./double-quote.svelte";
-export {default as OpenParenthesis} from "./open-parenthesis.svelte";
-export {default as CloseParenthesis} from "./close-parenthesis.svelte";
-export {default as Equal} from "./equal.svelte";
-export {default as Colon} from "./colon.svelte";
-export {default as Line} from "./line.svelte"
+export { default as Comma } from "./comma.svelte";
+export { default as SemiColon } from "./semicolon.svelte";
+export { default as OpenBracket } from "./open-bracket.svelte";
+export { default as CloseBracket } from "./close-bracket.svelte";
+export { default as OpenFlowerBracket } from "./open-flower-bracket.svelte";
+export { default as CloseFlowerBracket } from "./close-flower-bracket.svelte";
+export { default as SingleQuote } from "./single-quote.svelte";
+export { default as DoubleQuote } from "./double-quote.svelte";
+export { default as OpenParenthesis } from "./open-parenthesis.svelte";
+export { default as CloseParenthesis } from "./close-parenthesis.svelte";
+export { default as Equal } from "./equal.svelte";
+export { default as Colon } from "./colon.svelte";
+export { default as Line } from "./line.svelte"
