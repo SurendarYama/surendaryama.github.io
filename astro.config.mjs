bdc54b6a4501e0ecc6edbedf4f98444b94700785
Surendar Yama <surendaryama@outlook.com>
2025-05-04 12:39:32 +0530

    astro upgrade v5.7.10

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
+export default defineConfig(({
+  /** @type {import('astro').AstroUserConfig} */ // Comment out "renderers: []" to enable Astro's default component support.
   renderers: ["@astrojs/renderer-svelte"],
-});
+
+  integrations: [svelte()],
+}));
